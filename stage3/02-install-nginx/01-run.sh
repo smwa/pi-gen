45b102bc855cccb9565b1b1b481b436ed9036ab9
Michael Smith <michael.smith.ok@gmail.com>
2020-08-13 11:57:54 -0500

    Enable DNS Over TLS

--- stage3/02-install-nginx/01-run.sh
+++ stage3/02-install-nginx/01-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 cp -r files/nginx.conf "${ROOTFS_DIR}/etc/nginx/nginx.conf"
+cp -r files/selfsigned.crt "${ROOTFS_DIR}/etc/nginx/selfsigned.crt"
+cp -r files/selfsigned.key "${ROOTFS_DIR}/etc/nginx/selfsigned.key"
 mkdir -p "${ROOTFS_DIR}/var/www/"
 cp -r files/captive-portal "${ROOTFS_DIR}/var/www/captive-portal"
